ae026f2f9f569288d7b89b61c31a608e0467855e
Alevtina Muzychenko <fghbaim808@gmail.com>
2026-05-20 13:24:51 +1000

    Fix.

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,17 +2,20 @@ from django.contrib import admin
 
 from .models import Post, Category, Location
 
+
 @admin.register(Post)
 class PostAdminModel(admin.ModelAdmin):
     list_display = ['title', 'pub_date', 'category', 'is_published']
     search_fields = ['title']
     list_filter = ['is_published', 'category']
 
+
 @admin.register(Category)
 class CategoryAdminModel(admin.ModelAdmin):
     list_display = ['title', 'slug', 'is_published']
     search_fields = ['title']
     list_filter = ['is_published']
+
 
 @admin.register(Location)
 class LocationAdminModel(admin.ModelAdmin):
